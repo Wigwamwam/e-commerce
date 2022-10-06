71c61d149a6724bbce83ddc809d368a52bb96b75
George Wiggin <george@sequest.io>
2022-10-06 17:55:09 +0100

    addressing routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'projects/index'
-  get 'projects/show'
   devise_for :users
+  root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  # resources :projects, only: %i[index show]
   resources :projects, only: %i[index show]
+  resources :users, only: %i[show]
+  resources :checkouts, only: %i[index]
+
+
 
 
 
